db10b101140887c2cb024e2c8eeca9dea3ee6326
Ahmed Hendawy <ahmedmagdyahmed1996@outlook.com>
2020-06-19 01:35:38 +0200

    release update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ copyright = '2020, Ahmed Hendway - Bernhard Rolle'
 author = 'Ahmed Hendway - Bernhard Rolle'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
